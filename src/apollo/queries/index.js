3cf21e14a5ef64a4ac07725918dbaa71a9637de3
jesuskata <rzarate.alex@gmail.com>
2021-02-25 23:09:08 -0600

    feat: 🎸 create the graphql query to retrieve contentful data

---
+++ src/apollo/queries/index.js
@@ -0,0 +1,34 @@
+// Dependencies
+import { gql } from '@apollo/client';
+
+export const CardsQuery = gql`
+  query {
+    cardsCollection {
+      items {
+        title
+        image {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+        subtitle
+        caption
+        logo {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+      }
+    }
+  }
+`;
